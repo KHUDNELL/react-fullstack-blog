fd855c716c87093518f2f75f86b84304ea249774
Ken Hudnell <khudnell252@gmail.com>
2021-10-01 22:16:24 -0400

    setup pages directory, added react router

---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+    <h1>Hello, welcome to my blog!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum numquam, saepe ipsam commodi dolorum esse praesentium est nulla magni porro debitis quibusdam possimus reiciendis eum beatae error expedita ea earum.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum numquam, saepe ipsam commodi dolorum esse praesentium est nulla magni porro debitis quibusdam possimus reiciendis eum beatae error expedita ea earum.</p>
+    </>
+)
+
+export default HomePage;
